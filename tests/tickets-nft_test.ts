f9fe415f2a79f6c5e6b413a1c7c5e43706fb6a5c
mcqnyc <mcqnyc@gmail.com>
2021-11-02 21:20:28 -0400

    test: only owner can withdraw stx paid for nfts

--- tests/tickets-nft_test.ts
+++ tests/tickets-nft_test.ts
@@ -14,6 +14,7 @@ Clarinet.test({
     const recipient = accounts.get("wallet_1")!;
     const nftPrice = 100;
     const mintedNftId = "u1";
+
     const block = chain.mineBlock([
       Tx.contractCall("tickets-nft", "claim", [], recipient.address),
     ]);
@@ -34,20 +35,71 @@ Clarinet.test({
   name: "user can't get a token after the block-height has been reached",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     const recipient = accounts.get("wallet_1")!;
-    const targetBlockHeight = 10;
+    const unlockHeight = 10;
     chain.mineBlock([
       Tx.contractCall("tickets-nft", "claim", [], recipient.address),
     ]);
 
     // Advance the chain until the use-by block-height reached.
-    chain.mineEmptyBlockUntil(targetBlockHeight);
+    chain.mineEmptyBlockUntil(unlockHeight);
 
     const block = chain.mineBlock([
       Tx.contractCall("tickets-nft", "claim", [], recipient.address),
     ]);
     // Should return err-use-by-block-reached (err u102).
     block.receipts[0].result.expectErr().expectUint(102);
-    console.log("===> ", block.receipts);
+    assertEquals(block.receipts[0].events.length, 0);
+  },
+});
+
+Clarinet.test({
+  name: "owner can withdraw STX from contract",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    const nftPrice = 100;
+    const unlockHeight = 10;
+    const recipient = accounts.get("wallet_1")!;
+
+    chain.mineBlock([
+      Tx.contractCall("tickets-nft", "claim", [], recipient.address),
+    ]);
+    // Advance the chain until the use-by block-height reached.
+    chain.mineEmptyBlockUntil(unlockHeight);
+
+    const block = chain.mineBlock([
+      Tx.contractCall("tickets-nft", "withdraw-stx", [], deployer.address),
+    ]);
+
+    // The withdrawal should be successful.
+    block.receipts[0].result.expectOk().expectBool(true);
+
+    // There should be a STX transfer of the nftPrice specified.
+    block.receipts[0].events.expectSTXTransferEvent(
+      nftPrice,
+      `${deployer.address}.tickets-nft`,
+      deployer.address
+    );
+  },
+});
+
+Clarinet.test({
+  name: "no non-owner user can withdraw STX from contract",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const unlockHeight = 10;
+    const recipient = accounts.get("wallet_1")!;
+
+    chain.mineBlock([
+      Tx.contractCall("tickets-nft", "claim", [], recipient.address),
+    ]);
+    // Advance the chain until the use-by block-height reached.
+    chain.mineEmptyBlockUntil(unlockHeight);
+
+    const block = chain.mineBlock([
+      Tx.contractCall("tickets-nft", "withdraw-stx", [], recipient.address),
+    ]);
+
+    // The withdrawal should fail.
+    block.receipts[0].result.expectErr().expectUint(107);
     assertEquals(block.receipts[0].events.length, 0);
   },
 });
